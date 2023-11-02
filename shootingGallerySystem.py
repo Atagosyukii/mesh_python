e55c728dbbab2802acf5046d665d64573f3604c9
hi021012 <hi021012@thcu.ac.jp>
2023-11-02 18:29:13 +0900

    ログ出力のタイミングを変更した。

--- shootingGallerySystem.py
+++ shootingGallerySystem.py
@@ -140,13 +140,13 @@ async def on_receive_notify_BU(blockManager, _, data: bytearray):
             control_gpio_output_power(blockManager.get_gp_client2(), power_state=2)
         )
         operation_signal = False
-        await control_led(blockManager.get_le_client(), duration=1500, on=250, off=250, pattern=1, red=127, green=0, blue=0)
         print('System Pause.')
+        await control_led(blockManager.get_le_client(), duration=1500, on=250, off=250, pattern=1, red=127, green=0, blue=0)
     elif data[STATE_INDEX] == 1:  # ボタンが1回押されたことを判定し、システムを再開する
         if (operation_signal == True): return  # シグナルが動作中の場合はなにもしない
         operation_signal = True
+        print('System Resume.')
         await control_led(blockManager.get_le_client(), duration=3000, on=500, off=500, pattern=1, red=0, green=127, blue=0)
-        print('System Start.'),
 
 def on_receive(_, data: bytearray):
     return
